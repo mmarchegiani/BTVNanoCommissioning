48d766101549f9998365cc7462a6aab1dc40c791
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2021-03-19 14:55:21 +0100

    Plot ratio, flavor match

--- make_plots.py
+++ make_plots.py
@@ -6,7 +6,9 @@ import mplhep as hep
 from coffea.util import load
 from coffea.hist import plot
 import coffea.hist as hist
+import itertools
 import os
+
 
 parser = argparse.ArgumentParser(description='Plot histograms from coffea file')
 parser.add_argument('-i', '--input', type=str, help='Input histogram filename', required=True)
@@ -33,17 +35,17 @@ if not os.path.exists(plot_dir):
     os.makedirs(plot_dir)
 
 for histname in accumulator:
+    hist1d = not 'hist2d' in histname
     if histname in ["sumw", "nbtagmu", "nbtagmu_event_level"]: continue
-    if 'ccfatjet' in histname: continue
+    if not 'fatjet' in histname: continue
     print("Plotting", histname)
     #fig, ax = plt.subplots(1, 1, figsize=(12, 9))
-    fig, (ax, rax) = plt.subplots(2, 1, figsize=(12,12), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
-    fig.subplots_adjust(hspace=.07)
+    
     if any([histname.startswith('cutflow')]): break
     h = accumulator[histname]
     datasets = [str(s) for s in h.axis('dataset').identifiers() if str(s) is not 'dataset']
     mapping = {
-        'QCD_MuEnrichedPt5' : [dataset for dataset in datasets if 'QCD' in dataset],
+        'QCD_MuEnrichedPt5' : [dataset for dataset in datasets if 'QCD_Pt' in dataset],
         args.data : [args.data],
     }
     for dataset in datasets:
@@ -51,29 +53,95 @@ for histname in accumulator:
         if args.data in dataset: continue
         mapping[dataset] = [dataset]
     datasets = mapping.keys()
+    datasets_mc = [dataset for dataset in datasets if args.data not in dataset]
+
     h = h.group("dataset", hist.Cat("dataset", "Dataset"), mapping)
-    plot.plot1d(h[[dataset for dataset in datasets if args.data not in dataset]], ax=ax, legend_opts={'loc':1}, density=args.dense, stack=True)
-    plot.plot1d(h[args.data], ax=ax, legend_opts={'loc':1}, density=args.dense, error_opts=data_err_opts, clear=False)
-    plot.plotratio(num=h[args.data].sum('dataset'), denom=h[[dataset for dataset in datasets if args.data not in dataset]].sum('dataset'), ax=rax,
-                   error_opts=data_err_opts, denom_fill_opts={}, guide_opts={} )#, unc='num')
-    ax.set_yscale(args.scale)
-    rax.set_ylabel('data/MC')
-    rax.set_yscale(args.scale)
-    rax.set_ylim(0.000001,2)
-    at = AnchoredText(r"$\geq$1 AK8 jets"+"\n"+
-                          r"$p_T > 250 GeV$"+"\n"+
-                          r"$m_{SD} > 20 GeV$",
-                          loc=2, frameon=False)
-    ax.add_artist(at)
-    if histname.startswith("btag"):
-        ax.semilogy()
-    if not args.dense:
-        ax.set_ylim(0.001, None)
+    flavors = ['bb', 'cc', 'b', 'c', 'light']
+    if hist1d:
+        fig, (ax, rax) = plt.subplots(2, 1, figsize=(12,12), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
+        fig.subplots_adjust(hspace=.07)
+        plot.plot1d(h[datasets_mc].sum('flavor'), ax=ax, legend_opts={'loc':1}, density=args.dense, stack=True)
+        plot.plot1d(h[args.data].sum('flavor'), ax=ax, legend_opts={'loc':1}, density=args.dense, error_opts=data_err_opts, clear=False)
+        plot.plotratio(num=h[args.data].sum('dataset', 'flavor'), denom=h[datasets_mc].sum('dataset', 'flavor'), ax=rax,
+                       error_opts=data_err_opts, denom_fill_opts={}, guide_opts={}, unc='num')
+        ax.set_yscale(args.scale)
+        rax.set_ylabel('data/MC')
+        rax.set_yscale(args.scale)
+        rax.set_ylim(0.1,10)
+        at = AnchoredText(r"$\geq$1 AK8 jets"+"\n"+
+                              r"$p_T > 250 GeV$"+"\n"+
+                              r"$m_{SD} > 20 GeV$",
+                              loc=2, frameon=False)
+        ax.add_artist(at)
+        if histname.startswith("btag"):
+            ax.semilogy()
+        if not args.dense:
+            ax.set_ylim(0.001, None)
+        else:
+            plt.set_ylabel('a.u.')
+            ax.set_ylim(0.000001, None)
+        #hep.mpl_magic(ax)
+        filepath = plot_dir + histname + ".png"
+        if args.scale != parser.get_default('scale'):
+            filepath = filepath.replace(".png", "_" + args.scale + ".png")
+        print("Saving", filepath)
+        plt.savefig(filepath, dpi=300, format="png")
+        plt.close(fig)
+
+        if 'fatjet' in histname:
+            fig, (ax, rax) = plt.subplots(2, 1, figsize=(12,12), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
+            fig.subplots_adjust(hspace=.07)
+            #iterables = list(itertools.product(datasets_mc,flavors))   # Shortcut to avoid conisdering the flavor 'inclusive'
+            #print(h[('QCD_MuEnrichedPt5', ['b', 'c', 'cc'])].values())
+            #print(h[[(dataset, flavor) for dataset in datasets_mc for flavor in flavors]].values())
+            #plot.plot1d(h[[(dataset, flavor) for dataset in datasets_mc for flavor in flavors]].sum('dataset'), ax=ax, legend_opts={'loc':1}, density=args.dense, stack=True)
+            plot.plot1d(h[(datasets_mc, flavors)].sum('dataset'), ax=ax, legend_opts={'loc':1}, density=args.dense, order=flavors, stack=True)
+            plot.plot1d(h[args.data].sum('dataset', 'flavor'), ax=ax, legend_opts={'loc':1}, density=args.dense, error_opts=data_err_opts, clear=False)
+            plot.plotratio(num=h[args.data].sum('dataset', 'flavor'), denom=h[(datasets_mc, "inclusive")].sum('dataset', 'flavor'), ax=rax,
+                           error_opts=data_err_opts, denom_fill_opts={}, guide_opts={}, unc='num')
+            ax.set_yscale(args.scale)
+            #print(list(h[args.data].sum('dataset', 'flavor').values().values()))
+            #ax.set_ylim(0.001,1.05*max(np.array(h[args.data].sum('dataset', 'flavor').values().values())))
+            rax.set_ylabel('data/MC')
+            rax.set_yscale(args.scale)
+            rax.set_ylim(0.1,10)
+            at = AnchoredText(r"$\geq$1 AK8 jets"+"\n"+
+                                  r"$p_T > 250 GeV$"+"\n"+
+                                  r"$m_{SD} > 20 GeV$",
+                                  loc=2, frameon=False)
+            ax.add_artist(at)
+            if histname.startswith("btag"):
+                ax.semilogy()
+            if not args.dense:
+                ax.set_ylim(0.001, None)
+            else:
+                plt.set_ylabel('a.u.')
+                ax.set_ylim(0.000001, None)
+            #hep.mpl_magic(ax)
+            filepath = plot_dir + histname + ".png"
+            filepath = filepath.replace(".png", "_flavormatch.png")
+            if args.scale != parser.get_default('scale'):
+                filepath = filepath.replace(".png", "_" + args.scale + ".png")
+            print("Saving", filepath)
+            plt.savefig(filepath, dpi=300, format="png")
+            plt.close(fig)
     else:
-        plt.set_ylabel('a.u.')
-        ax.set_ylim(0.000001, None)
-    #hep.mpl_magic(ax)
-    filepath = plot_dir + histname + ".png"
-    if args.scale != parser.get_default('scale'):
-    	filepath.replace(".png", "_" + args.scale + ".png")
-    plt.savefig(plot_dir + histname + ".png", dpi=300, format="png")
+        for dataset in datasets:
+            if 'QCD' in dataset:
+                histo_QCD = h[dataset].sum('dataset')
+            if 'GluGluHToBB' in dataset:
+                histo_BB = h[dataset].sum('dataset')
+            if 'GluGluHToCC' in dataset:
+                histo_CC = h[dataset].sum('dataset')
+        xaxis = [axis.name for axis in histo_QCD.axes() if 'btag' in axis.name][0]
+
+        for histo_GluGlu, dataset_GluGlu in zip([histo_BB, histo_CC], ['GluGluHToBB', 'GluGluHToCC']):
+            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(24,9))
+            plot.plot2d(histo_QCD.sum('flavor'), xaxis=xaxis, ax=ax1)
+            plot.plot2d(histo_GluGlu.sum('flavor'), xaxis=xaxis, ax=ax2)
+            ax1.set_title('QCD')
+            ax2.set_title(dataset_GluGlu)
+            filepath = plot_dir + histname + "_" + dataset_GluGlu + ".png"
+            print("Saving", filepath)
+            plt.savefig(filepath, dpi=300, format="png")
+            plt.close(fig)
