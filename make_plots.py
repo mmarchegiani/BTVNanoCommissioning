0549cdca586d01532f56c0923aaf428fefa0d3f9
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2021-04-20 18:31:32 +0200

    Nice plots

--- make_plots.py
+++ make_plots.py
@@ -8,7 +8,7 @@ from coffea.hist import plot
 import coffea.hist as hist
 import itertools
 import os
-
+from utils import histogram_settings
 
 parser = argparse.ArgumentParser(description='Plot histograms from coffea file')
 parser.add_argument('-i', '--input', type=str, help='Input histogram filename', required=True)
@@ -29,6 +29,26 @@ data_err_opts = {
     'elinewidth': 1,
 }
 
+qcd_opts = {
+    'facecolor': 'yellow',
+    'edgecolor': 'black',
+    'alpha': 1.0
+}
+
+signal_opts = {
+    'facecolor': 'None',
+    'edgecolor': ['green', 'red'],
+    'linestyle': ['--', '-'],
+    'linewidth': 2,
+    'alpha': 0.7
+}
+
+flavor_opts = {
+    'facecolor': ['cyan', 'magenta', 'red', 'green', 'blue'],
+    'edgecolor': 'black',
+    'alpha': 1.0
+}
+
 plt.style.use([hep.style.ROOT, {'font.size': 16}])
 plot_dir = "plots/" + args.output + "/"
 if not os.path.exists(plot_dir):
@@ -44,9 +64,13 @@ for histname in accumulator:
     
     if any([histname.startswith('cutflow')]): break
     h = accumulator[histname]
+    if histname in histogram_settings['variables'].keys():
+        varname = h.fields[-1]
+        varlabel = h.axis(varname).label
+        h = h.rebin(varname, hist.Bin(varname, varlabel, **histogram_settings['variables'][histname]['binning']))
     datasets = [str(s) for s in h.axis('dataset').identifiers() if str(s) is not 'dataset']
     mapping = {
-        'QCD_MuEnrichedPt5' : [dataset for dataset in datasets if 'QCD_Pt' in dataset],
+        r'QCD ($\mu$ enriched)' : [dataset for dataset in datasets if 'QCD_Pt' in dataset],
         args.data : [args.data],
     }
     for dataset in datasets:
@@ -54,22 +78,39 @@ for histname in accumulator:
         if args.data in dataset: continue
         mapping[dataset] = [dataset]
     datasets = mapping.keys()
-    datasets_mc = [dataset for dataset in datasets if args.data not in dataset]
+    #datasets_mc  = [dataset for dataset in datasets if args.data not in dataset]
+    datasets_QCD = [dataset for dataset in datasets if ((args.data not in dataset) & ('GluGlu' not in dataset))]
+    datasets_ggH = [dataset for dataset in datasets if 'GluGlu' in dataset]
 
     h = h.group("dataset", hist.Cat("dataset", "Dataset"), mapping)
-    #flavors = ['bb', 'cc', 'b', 'c', 'light']
-    flavors = ['bb', 'cc', 'b', 'c', 'light', 'others']
+    flavors = ['bb', 'cc', 'b', 'c', 'light']
+    #flavors = ['bb', 'cc', 'b', 'c', 'light', 'others']
     if hist1d:
         fig, (ax, rax) = plt.subplots(2, 1, figsize=(12,12), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
         fig.subplots_adjust(hspace=.07)
-        plot.plot1d(h[datasets_mc].sum('flavor'), ax=ax, legend_opts={'loc':1}, density=args.dense, stack=True)
+        plot.plot1d(h[datasets_QCD].sum('flavor'), ax=ax, legend_opts={'loc':1}, density=args.dense, fill_opts=qcd_opts, stack=True)
+        ggH_rescaled = h[datasets_ggH].sum('flavor')
+        scale_ggH = 1000
+        ggH_rescaled.scale(scale_ggH)
+        ax_ggH = plot.plot1d(ggH_rescaled, ax=ax, legend_opts={'loc':1}, density=args.dense, fill_opts=signal_opts, stack=False, clear=False)
         plot.plot1d(h[args.data].sum('flavor'), ax=ax, legend_opts={'loc':1}, density=args.dense, error_opts=data_err_opts, clear=False)
-        plot.plotratio(num=h[args.data].sum('dataset', 'flavor'), denom=h[datasets_mc].sum('dataset', 'flavor'), ax=rax,
+        plot.plotratio(num=h[args.data].sum('dataset', 'flavor'), denom=h[datasets_QCD].sum('dataset', 'flavor'), ax=rax,
                        error_opts=data_err_opts, denom_fill_opts={}, guide_opts={}, unc='num')
+        handles, labels = ax.get_legend_handles_labels()
+        for (i, label) in enumerate(labels):
+            if "GluGlu" in label:
+                if "BB" in label:
+                    labels[i] = r"ggH$\rightarrow$bb $\times$" + str(scale_ggH)
+                if "CC" in label:
+                    labels[i] = r"ggH$\rightarrow$cc $\times$" + str(scale_ggH)
+        ax.legend(handles, labels)
         ax.set_yscale(args.scale)
-        rax.set_ylabel('data/MC')
-        rax.set_yscale(args.scale)
-        rax.set_ylim(0.1,3)
+        rax.set_ylabel('Data/MC')
+        #.rax.set_yscale(args.scale)
+        rax.set_ylim(0.5,1.5)
+        if histname in histogram_settings['variables'].keys():
+            ax.set_xlim(**histogram_settings['variables'][histname]['xlim'])
+            rax.set_xlim(**histogram_settings['variables'][histname]['xlim'])
         at = AnchoredText(r"$\geq$1 AK8 jets"+"\n"+
                           r"$p_T > 250 GeV$"+"\n"+
                           r"$m_{SD} > 20 GeV$"+"\n"+
@@ -79,11 +120,11 @@ for histname in accumulator:
         if histname.startswith("btag"):
             ax.semilogy()
         if (not args.dense) & (args.scale == "log"):
-            ax.set_ylim(0.00001, None)
+            ax.set_ylim(0.1, 10**7)
         #hep.mpl_magic(ax)
         filepath = plot_dir + histname + ".png"
         if args.scale != parser.get_default('scale'):
-            rax.set_ylim(0.1,10)
+            #rax.set_ylim(0.1,10)
             filepath = filepath.replace(".png", "_" + args.scale + ".png")
         print("Saving", filepath)
         plt.savefig(filepath, dpi=300, format="png")
@@ -92,20 +133,23 @@ for histname in accumulator:
         #if 'fatjet' in histname:
         fig, (ax, rax) = plt.subplots(2, 1, figsize=(12,12), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
         fig.subplots_adjust(hspace=.07)
-        #iterables = list(itertools.product(datasets_mc,flavors))   # Shortcut to avoid conisdering the flavor 'inclusive'
+        #iterables = list(itertools.product(datasets_QCD,flavors))   # Shortcut to avoid conisdering the flavor 'inclusive'
         #print(h[('QCD_MuEnrichedPt5', ['b', 'c', 'cc'])].values())
-        #print(h[[(dataset, flavor) for dataset in datasets_mc for flavor in flavors]].values())
-        #plot.plot1d(h[[(dataset, flavor) for dataset in datasets_mc for flavor in flavors]].sum('dataset'), ax=ax, legend_opts={'loc':1}, density=args.dense, stack=True)
-        plot.plot1d(h[(datasets_mc, flavors)].sum('dataset'), ax=ax, legend_opts={'loc':1}, density=args.dense, order=flavors, stack=True)
+        #print(h[[(dataset, flavor) for dataset in datasets_QCD for flavor in flavors]].values())
+        #plot.plot1d(h[[(dataset, flavor) for dataset in datasets_QCD for flavor in flavors]].sum('dataset'), ax=ax, legend_opts={'loc':1}, density=args.dense, stack=True)
+        plot.plot1d(h[(datasets_QCD, flavors)].sum('dataset'), ax=ax, legend_opts={'loc':1}, density=args.dense, fill_opts=flavor_opts, order=flavors, stack=True)
         plot.plot1d(h[args.data].sum('dataset'), ax=ax, legend_opts={'loc':1}, density=args.dense, error_opts=data_err_opts, clear=False)
-        plot.plotratio(num=h[args.data].sum('dataset', 'flavor'), denom=h[(datasets_mc, flavors)].sum('dataset', 'flavor'), ax=rax,
+        plot.plotratio(num=h[args.data].sum('dataset', 'flavor'), denom=h[(datasets_QCD, flavors)].sum('dataset', 'flavor'), ax=rax,
                        error_opts=data_err_opts, denom_fill_opts={}, guide_opts={}, unc='num')
         ax.set_yscale(args.scale)
         #print(list(h[args.data].sum('dataset', 'flavor').values().values()))
         #ax.set_ylim(0.001,1.05*max(np.array(h[args.data].sum('dataset', 'flavor').values().values())))
-        rax.set_ylabel('data/MC')
-        rax.set_yscale(args.scale)
-        rax.set_ylim(0.1,3)
+        rax.set_ylabel('Data/MC')
+        #rax.set_yscale(args.scale)
+        rax.set_ylim(0.5,1.5)
+        if histname in histogram_settings['variables'].keys():
+            ax.set_xlim(**histogram_settings['variables'][histname]['xlim'])
+            rax.set_xlim(**histogram_settings['variables'][histname]['xlim'])
         at = AnchoredText(r"$\geq$1 AK8 jets"+"\n"+
                           r"$p_T > 250 GeV$"+"\n"+
                           r"$m_{SD} > 20 GeV$"+"\n"+
@@ -115,12 +159,12 @@ for histname in accumulator:
         if histname.startswith("btag"):
             ax.semilogy()
         if (not args.dense) & (args.scale == "log"):
-            ax.set_ylim(0.0000001, 1000000)
+            ax.set_ylim(0.1, 10**7)
         #hep.mpl_magic(ax)
         filepath = plot_dir + histname + ".png"
         filepath = filepath.replace(".png", "_flavormatch.png")
         if args.scale != parser.get_default('scale'):
-            rax.set_ylim(0.1,10)
+            #rax.set_ylim(0.1,10)
             filepath = filepath.replace(".png", "_" + args.scale + ".png")
         print("Saving", filepath)
         plt.savefig(filepath, dpi=300, format="png")
@@ -129,10 +173,14 @@ for histname in accumulator:
         for dataset in datasets:
             if 'QCD' in dataset:
                 histo_QCD = h[dataset].sum('dataset')
+                histo_QCD_bb = h[dataset].sum('dataset')['bb']
+                histo_QCD_cc = h[dataset].sum('dataset')['cc']
             if 'GluGluHToBB' in dataset:
-                histo_BB = h[dataset].sum('dataset')
+                histo_BB    = h[dataset].sum('dataset')
+                histo_BB_bb = h[dataset].sum('dataset')['bb']
             if 'GluGluHToCC' in dataset:
-                histo_CC = h[dataset].sum('dataset')
+                histo_CC    = h[dataset].sum('dataset')
+                histo_CC_cc = h[dataset].sum('dataset')['cc']
         xaxis = [axis.name for axis in histo_QCD.axes() if 'btag' in axis.name][0]
 
         for histo_GluGlu, dataset_GluGlu in zip([histo_BB, histo_CC], ['GluGluHToBB', 'GluGluHToCC']):
@@ -142,6 +190,23 @@ for histname in accumulator:
             ax1.set_title('QCD')
             ax2.set_title(dataset_GluGlu)
             filepath = plot_dir + histname + "_" + dataset_GluGlu + ".png"
+            print("Saving", filepath)
+            plt.savefig(filepath, dpi=300, format="png")
+            plt.close(fig)
+
+        for histo_GluGlu_xx, dataset_GluGlu_xx in zip([histo_BB_bb, histo_CC_cc], ['GluGluHToBB (bb)', 'GluGluHToCC (cc)']):
+            if dataset_GluGlu_xx == 'GluGluHToBB (bb)':
+                histo_QCD_xx = histo_QCD_bb
+                qcd_label = 'QCD (bb)'
+            elif dataset_GluGlu_xx == 'GluGluHToCC (cc)':
+                histo_QCD_xx = histo_QCD_cc
+                qcd_label = 'QCD (cc)'
+            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(24,9))
+            plot.plot2d(histo_QCD_xx.sum('flavor'), xaxis=xaxis, ax=ax1)
+            plot.plot2d(histo_GluGlu_xx.sum('flavor'), xaxis=xaxis, ax=ax2)
+            ax1.set_title(qcd_label)
+            ax2.set_title(dataset_GluGlu_xx)
+            filepath = plot_dir + histname + "_" + '_'.join(dataset_GluGlu_xx.strip(')').split(' (')) + ".png"
             print("Saving", filepath)
             plt.savefig(filepath, dpi=300, format="png")
             plt.close(fig)
